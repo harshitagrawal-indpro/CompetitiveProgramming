30194a422a932b842ca7a8803d83ecf6f7ed07ca
harshitagrawal-indpro <harshitagra8092@gmail.com>
2025-05-04 06:43:54 +0530

    recursion1

---
+++ Recursion/1toN.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void prints(int i, int j){
+    if (i==j+1){
+        return;
+    }
+    else {
+        cout << i << endl;
+        prints(i+1,j);
+    }
+}
+int main(){
+    int i=1 , j=5;
+    prints(i,j);
+    return 0;
+}
