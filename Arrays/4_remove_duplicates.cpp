dd8e1a057626bc51f1bb20a1d158f387a5741640
harshitagrawal-indpro <harshitagra8092@gmail.com>
2025-05-17 23:59:47 +0530

    added remove duplicates add1

--- Arrays/4_remove_duplicates.cpp
+++ Arrays/4_remove_duplicates.cpp
@@ -3,4 +3,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){}
+int main(){
+    int n;
+    cin >> n ;
+    int arr[n];
+    for (int i =0 ; i<n ; i++){
+        cin >> arr[i];
+    }
+    
+}
