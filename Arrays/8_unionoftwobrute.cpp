81afeddb47982e45a893fdaffab911e6a05b356e
harshitagrawal-indpro <harshitagra8092@gmail.com>
2025-05-23 03:06:30 +0530

    change

--- Arrays/8_unionoftwobrute.cpp
+++ Arrays/8_unionoftwobrute.cpp
@@ -9,7 +9,9 @@ int main(){
     cin >>  n >> m ;
     int k=m+n;
     int a[n];int b[m];vector <int> ve;
+    set <int> s ;
 
+    // taking inputs
     for (int i=0 ; i<n ; i++){
         cin >> a[i];
     }
@@ -17,19 +19,22 @@ int main(){
         cin >> b[i];
     }
 
-    set <int> s ;
+
+    // MAKING SET FROM BOTH ARRAYS 
     for (int i=0 ; i<n ; i++){
-        s.insert(a[i]);
+        s.insert(a[i]); // O(N LOG N)
     }
     for (int i=0 ; i<m ; i++){
-        s.insert(b[i]);
+        s.insert(b[i]); // O(M LOG M)
     }
 
     for (int i : s){
-        ve.push_back(i);
+        ve.push_back(i); // O(N+M)
     }
 
     for (auto i: ve){
         cout << i << " ";
     }
 }
+
+// tc - O(N+M)log(M+N) SC = O(M+N)
