c994ffeb3d9fb2e8225f9d29f914bfb4c57ba855
harshitagrawal-indpro <harshitagra8092@gmail.com>
2025-05-23 02:42:21 +0530

    union_of_setsoptimised

--- Arrays/8_unionoftwo.cpp
+++ Arrays/8_unionoftwo.cpp
@@ -7,41 +7,38 @@ int main(){
     int a=3 ; int b=5;
     int arr1[a+b];
     int i=0, j=0,k=0;
-    while (i<a && j<b){
-        if (arr[i]<arr[j]){
-            if (k> 0 && arr[i]==arr[k-1]){
-                i++;
-            }else{
-                arr1[k]=arr[i]; k++; i++;
+
+    while(i<a && j<b){
+        if (arr[i]<brr[j]){
+            if (!(k>0 && arr1[k-1]==arr[i])){
+                arr1[k]=arr[i];
+                k++;
             }
-        }else if (arr[i]>arr[j]){
-            if (k> 0 && arr[j]==arr[k-1]){
-                j++;
-            }else{
-                arr1[k]=arr[j]; k++; j++;
+            i++;
+        }else {
+            if (!(k>0 && arr1[k-1]==arr[j])){
+                arr1[k]=brr[j];
+                k++;
             }
-        }else{
-            if (k> 0 && arr[i]==arr[k-1]){
-                i++; j++;
-            }else{
-                arr1[k]=arr[i]; k++; i++; j++;
-            }
+            j++;
         }
     }
-    while (i<a){
-        if (arr[i]==arr[k-1]){
+    while(i < a ){
+        if (!(k>0 && arr1[k-1]==arr[i])){
+                arr1[k]=arr[i];
+                k++;
+            }
             i++;
-        }else{
-            arr1[k]=arr[i]; k++; i++;
-        }
     }
     while (j<b){
-        if (k> 0 && arr[j]==arr[k-1]){
+        if (!(k>0 && arr1[k-1]==arr[j])){
+                arr1[k]=brr[j];
+                k++;
+            }
             j++;
-        }else{
-            arr1[k]=arr[j]; k++; j++;
-        }
     }
+
+
     for (int i=0;i<k;i++){
         cout<<arr1[i]<<" ";
     }
