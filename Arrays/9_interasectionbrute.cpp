034552789c6d4a643d2e6fe8f22a082558e9afbc
harshitagrawal-indpro <harshitagra8092@gmail.com>
2025-05-24 21:54:25 +0530

    change

---
+++ Arrays/9_interasectionbrute.cpp
@@ -0,0 +1,17 @@
+// to intersect two arrays using brute force method 
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n , m ;
+    int arr1[n], arr2[m];
+    cin >> n >> m ;
+    for (int i=0 ; i<n ;i++){
+        cin >> arr1[i];
+    }
+    for (int i=0; i<m; i++){
+        cin >> arr2[i];
+    }
+
+    vector <int> temp(5,0);
+    
+}
