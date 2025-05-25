adb4aa8bd270b19fc12b2d719d0a4c357e3b5bf4
harshitagrawal-indpro <harshitagra8092@gmail.com>
2025-05-25 22:31:16 +0530

    changes

---
+++ Arrays/5_movezeroesatfront.cpp
@@ -0,0 +1,16 @@
+// move zeroes at front 
+
+void moveZerosToFront(int arr[], int n){
+        int j= n-1;
+        for (int i=n-1 ; i>=0 ; i--){
+            if (arr[i]!=0){
+                arr[j]=arr[i];
+                j--;
+            }
+        }
+        for (int i=j; i>=0 ; i--){
+            arr[i]=0;
+        }
+        
+        
+    }
