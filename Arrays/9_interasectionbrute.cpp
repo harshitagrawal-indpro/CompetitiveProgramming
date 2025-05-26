f3f9b5bbebd4a51202a25973943262a5710db293
harshitagrawal-indpro <harshitagra8092@gmail.com>
2025-05-26 07:31:55 +0530

    intersectionbrute

--- Arrays/9_interasectionbrute.cpp
+++ Arrays/9_interasectionbrute.cpp
@@ -1,17 +1,35 @@
-// to intersect two arrays using brute force method 
+// intersection brute algorithmn
+// by Harshit Agrawal 2
+
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
-    int n , m ;
-    int arr1[n], arr2[m];
-    cin >> n >> m ;
-    for (int i=0 ; i<n ;i++){
-        cin >> arr1[i];
-    }
-    for (int i=0; i<m; i++){
-        cin >> arr2[i];
-    }
 
-    vector <int> temp(5,0);
-    
+int main() {
+  int m, n ;
+  cin >> n >> m;
+  int arr1[n], arr2[m];
+  for (int i=0; i<n ; i++){
+      cin >> arr1[i];
+  }
+  for (int i=0; i<m ; i++){
+      cin >> arr2[i];
+  }
+
+  vector <int> temp(m,0);
+  
+  for (int i=0;i<n;i++){
+      for(int j=0;j<m;j++ ){
+          if(arr1[i]==arr2[j] && temp[j]==0){
+              temp[j]=1;
+          }
+      }
+      
+      }
+  
+  for (int i=0;i<m;i++ ){
+      if (temp[i]!=0){
+          cout<< arr2[i] << endl;
+      }
+  }
+  return 0;
 }
