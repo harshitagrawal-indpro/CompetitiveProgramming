9195b7ccfdc9a310443c1796c194b1f633f07a82
harshitagrawal-indpro <harshitagra8092@gmail.com>
2025-05-30 21:40:26 +0530

    temp hash

--- Arrays/10_maxconsecutive.cpp
+++ Arrays/10_maxconsecutive.cpp
@@ -1,1 +1,23 @@
 // its a good method to solve and fine the missing number 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n ;
+  int arr[n],temp[n+1];
+  for(int i=0 ; i<n-1 ; i++){
+      cin >> arr[i];
+  }
+  for (int i =0; i<n-1 ; i++){
+      temp[arr[i]]= 1;
+  }
+  for (int i=1; i<n+1; i++){
+      if (temp[i]==0){
+          cout << i <<endl;
+      }
+  }
+  
+  return 0;
+}
