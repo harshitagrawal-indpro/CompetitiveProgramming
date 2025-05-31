a6eda7a4bd2011571ab5e34a606173d488f75b6e
harshitagrawal-indpro <harshitagra8092@gmail.com>
2025-05-31 06:32:30 +0530

    find single element in array better one

---
+++ Arrays/11_findingsingleelementfromarraybetter.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+  // bruteforce method 
+  int n ;
+  cin >> n ;
+  int arr[n];
+  for (int i =0 ;i < n ; i++){
+      cin >> arr[i];
+  }
+  int max =arr[0];
+  for (int i=0 ; i<n ; i++){
+      if (arr[i]> max ){
+          max = arr[i];
+      }
+  }
+  vector <int> temp(max+1,0);
+  for (int i=0 ; i<n ; i++){
+     temp[arr[i]]+=1;
+  }
+  for (int i=0; i<max+1; i++){
+      if (temp[i]==1){
+          cout << i << endl;
+      }
+  }
+  return 0;
+}
