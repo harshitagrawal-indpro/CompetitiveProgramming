1775b1a3ee7f9f161961466ee04539d71b4b0556
harshitagrawal-indpro <harshitagra8092@gmail.com>
2025-05-31 06:55:31 +0530

    find single element in array optimised

--- Arrays/11_findingsingleelementfromarraygood.cpp
+++ Arrays/11_findingsingleelementfromarraygood.cpp
@@ -2,18 +2,18 @@
 using namespace std;
 
 int main() {
-  // best method 
+  // best method using sorted maps 
   int n ;
   cin >> n ;
   int arr[n];
   for (int i =0 ;i < n ; i++){
       cin >> arr[i];
   }
-  map <int , int > m ;
+  map <int , int > m ; // nlog(x) where x is size of map and x = (n+1)/2 for 1 element takes lox x and n takes n log x 
   for (int i=0 ; i<n ; i++){
       m[arr[i]]+=1;
   }
-  for (auto i : m){
+  for (auto i : m){ // takes x times complexity 
       cout << i.first << ":" << i.second << endl;
       if (i.second==1){
           cout<< i.first << " is single element!" << endl;
