24951383192cbb19de4166e649d03800795fa92a
harshitagrawal-indpro <harshitagra8092@gmail.com>
2025-05-31 06:22:44 +0530

    find single element in array

---
+++ Arrays/11_findsingleelementinarray.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+  // bruteforce method 
+  int n ;
+  cin >> n ;
+  int arr[n];
+  for (int i =0 ;i < n ; i++){
+      cin >> arr[i];
+  }
+  for (int i=0 ; i< n ; i++){
+      int count =0;
+      for (int j =0 ; j<n;j++){
+          if (arr[j]==arr[i]){
+              count++;
+          }
+      }
+      if (count==1){
+          cout << arr[i] << endl;
+      }
+  }
+  return 0;
+}
